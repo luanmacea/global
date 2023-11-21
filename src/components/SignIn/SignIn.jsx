b369d0c1ed446c3ec1c150c428b53b76918ad0c5
luanmacea <luanmacea@gmail.com>
2023-11-21 17:17:49 -0300

    peparacoes cadastro

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignIn.css";
 
 export default function SignIn() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [password, setPassword] = useState("");
+  const [verify, setVerify] = useState("");
+
+  const emailRegExp = /\S+@\S+\.\S{3,}/;
+
+  const validarFormulario = () => {
+    if (
+      nome === "" ||
+      telefone === "" ||
+      email === "" ||
+      password === "" ||
+      verify === ""
+    ) {
+      alert(
+        "Por favor, preencha todos os campos (nome, telefone, email, senha e confirmação)."
+      );
+      return false;
+    } else if (!emailRegExp.test(email)) {
+      alert("Por favor, informe um e-mail válido.");
+      return false;
+    } else if (nome.length < 2 || nome.length > 30) {
+      alert("O campo nome não atingiu o número mínimo de caracteres.");
+      return false;
+    } else if (telefone.length < 6) {
+      alert("Digite um telefone válido (Utilize DDD).");
+      return false;
+    } else if (password.length < 6) {
+      alert("A senha deve ter no mínimo 6 digitos.");
+      return false;
+    } else if (password !== verify) {
+      alert("A senha e a confirmação devem ser iguais.");
+      return false;
+    } else {
+      return true;
+    }
+  };
   return (
     <div
       style={{
@@ -24,6 +63,7 @@ export default function SignIn() {
             style={{ width: "100%" }}
             id="formGroupExampleInput"
             placeholder="Digite seu Nome"
+            onChange={(e) => setNome(e.target.value)}
           />
         </div>
         <div className="mb-1">
@@ -35,6 +75,7 @@ export default function SignIn() {
             className="form-control me-5 mb-2"
             id="formGroupExampleInput2"
             placeholder="Digite sua Email"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="mb-1">
@@ -47,6 +88,7 @@ export default function SignIn() {
             style={{ width: "100%" }}
             id="formGroupExampleInput"
             placeholder="Digite seu Telefone"
+            onChange={(e) => setTelefone(e.target.value)}
           />
         </div>
         <div className="mb-1">
@@ -59,6 +101,7 @@ export default function SignIn() {
             style={{ width: "100%" }}
             id="formGroupExampleInput"
             placeholder="Digite sua Senha"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="mb-3">
