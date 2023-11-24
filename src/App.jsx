ce3f416a08b3d1cac67c7c02bb2c7134f98c932c
Gabriel Machado Belardino <RM550121@fiap.com.br>
2023-11-24 12:31:26 -0300

    feat(App): add container to for the content of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Route, HashRouter as Router, Routes, Outlet } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import "./App.scss";
 
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
@@ -9,13 +9,15 @@ import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<div className="container-content">
+				<Outlet />
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
